540d6d8087f32cb3976e14932463b418b8e2f334
Ratu Atikah N <ratuuatikahn30@gmail.com>
2022-02-26 21:24:20 +0700

    Hasil Tugas Praktikum Ratu Atikah N

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+//Tugas Percobaan 1
+import 'package:flutter_basic/buildinwidget/textwidget.dart';
+//Tugas Percobaan 2
+import 'package:flutter_basic/buildinlayout/propertyalignment.dart';
+//Tugas Percobaan 3
+import 'package:flutter_basic/rowdancolumn/rowwidget.dart';
+//Tugas Percobaan 4 Stack
+import 'package:flutter_basic/stack/stack.dart';
+// Tugas Percobaan ListView
+import 'package:flutter_basic/listview/listview.dart';
+//Tugas Percobaan Grid View
+import 'package:flutter_basic/gridview/gridview.dart';
+//Tugas Praktikum
+import 'package:flutter_basic/tugasprak.dart';
+
+void main() {
+  runApp(const Praktikum());
+}
